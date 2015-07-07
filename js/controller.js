f52e0b6d4d7bd22e809bace96d0e56aae6fe6e3a
pawelqbera <pawel.kubera@chatwee.com>
2015-07-08 00:30:29 +0200

    Updating my js files, uploading files is ready.

--- js/controller.js
+++ js/controller.js
@@ -4,21 +4,19 @@
 	/**
 	* Sets up the controller for our view
 	*/
-
 	var Controller = function(view) {
 		this.view = view;
 		var _that = this;
 		var uploadBtn = document.getElementById("upload-btn");
 
-		//Event Listeners
-
 	 	uploadBtn.addEventListener("click", function() {
 	 		_that.openFileDialog();
 	 	});
-
-
 	};
 
+	/**
+	* Fire the click event on a pseudo-file input and open the multiple file dialog 
+	*/
 	Controller.prototype.openFileDialog = function() {
 		var _that = this;
 
@@ -28,25 +26,75 @@
 		this.view._addAttribute("pseudo-file-input", "multiple", "multiple");
 
  		document.getElementById("pseudo-file-input").click();
- 		
- 		document.getElementById('pseudo-file-input').addEventListener('change', function () {
+ 		document.getElementById("pseudo-file-input").addEventListener("change", function () {
  			_that.uploadImage(this);
  		});
 
  		this.view._removeElement("pseudo-container");
 	};
 
-	Controller.prototype.uploadImage = function() {
-
- 		for (i = 0; i < file.files.length; i++) {
-			
- 			renderImage(file.files[i]);
-
+	/**
+	* Start uploading all selected files 
+	*/
+	Controller.prototype.uploadImage = function(file) {
+ 		for (var i = 0; i < file.files.length; i++) {			
+ 			this.renderImage(file.files[i]);
  		}		
 	};
 
-	/*
-	*	Hook up our view to window object
+	/**
+	* Instantiates a FileReader() object coresponding to every single uploaded image 
+	*/
+	Controller.prototype.renderImage = function(file) {
+ 		this.reader = new FileReader();
+ 		this.reader.onload = this.createThumb;
+ 		this.reader.onerror = this.errorHandler; 
+ 		this.reader.readAsDataURL(file);
+	};
+
+	/**
+	* Creating imgage tags with sources coressponding to all uploaded images 
+	* and appending them to a container as thumbnails 
+	*/
+	Controller.prototype.createThumb = function(e) {		
+		var _that = this;		
+
+		var thumbnailsContainer = document.getElementById("thumbs-container");
+		var url = e.target.result;
+
+ 		this.thumb = document.createElement("img");
+ 		this.thumb.setAttribute("class", "thumb");
+ 		this.thumb.setAttribute("src", url);
+ 		thumbnailsContainer.appendChild(this.thumb);
+
+		this.thumb.addEventListener('click', function() {
+			_that.thumbnailClick(this);
+		});
+ 	};
+
+	/**
+	* Handles the click event on a thumbnail by openning the image in original
+	* sizes in separate browser's window
+	*/
+	FileReader.prototype.thumbnailClick = function(thumbnail) {	
+		this.thumbnail = thumbnail;
+		var url = thumbnail.getAttribute("src");
+ 		var win = window.open("", "Title", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=580, height=200");		
+ 		win.document.body.innerHTML = ("<img src='" + url + "' />");
+ 	};
+
+	/**
+	* Handles all upload errors resulted from broken and not-readable image files 
+	*/
+	Controller.prototype.errorHandler = function(e) {
+
+		if(e.target.error.name == "NotReadableError") {		
+			console.log("The file could not be read");		
+		}
+	};
+
+	/**
+	* Hook up our controller to window object
 	*/	
 	window.Controller = Controller;
 
--- js/view.js
+++ js/view.js
@@ -6,35 +6,13 @@
 	*/
 
 	var View = function(parentId) {
-
 		this.parentId = parentId;
 
-		this._createElement("section", "uploader-js", this.parentId);
-		
-		this._createElement("header", "header", "uploader-js");
-		this._createElement("section", "main", "uploader-js");
-
-		this._createElement("h1", "title", "header");
-		this._appendTextNode("title", "UploaderJS: Upload a file");
-
-		this._createElement("div", "thumbs-container", "main");
-		this._createElement("div", "progress-bar", "thumbs-container");
-
-		this._createElement("div", "droper", "main");
-		this._createElement("h2", "subtitle", "droper");
-		this._appendTextNode("subtitle", "Drop your .png or .jpg files here");
-
-		this._createElement("div", "file-uploader", "main");
-		this._createElement("span", "or", "file-uploader");
-		this._appendTextNode("or", "or");
-		this._createElement("input", "upload-btn", "file-uploader");
-		this._addAttribute("upload-btn", "type", "button");
-		this._addAttribute("upload-btn", "name", "upload-btn");
-		this._addAttribute("upload-btn", "value", "Select Files");
+		this.buildInterface(this.parentId);
 	};
 
-	/*
-	*	Creates and appends a DOM element with its own classname, id into a specified parent element 
+	/**
+	* Creates and appends a DOM element with its own classname, id into a specified parent element 
 	*/	
 	View.prototype._createElement = function(elementTag, elementName, parentId) {
 		this.elementTag = elementTag;
@@ -53,8 +31,8 @@
 		}
 	}
 
-	/*
-	*	Appends text into a specified element 
+	/**
+	* Appends text into a specified element 
 	*/
 	View.prototype._appendTextNode = function(elementId, elementText) {
 		this.elementId = elementId;
@@ -65,8 +43,8 @@
 		el.innerHTML = this.elementText;
 	}
 
-	/*
-	*	Appends text into a specified element 
+	/**
+	* Appends text into a specified element 
 	*/
 	View.prototype._addAttribute = function(elementId, elementAttribute, attributeValue) {
 		this.elementId = elementId;
@@ -78,8 +56,8 @@
 		el.setAttribute(this.elementAttribute, this.attributeValue);
 	}
 
-	/*
-	*	Remove element from DOM 
+	/**
+	* Remove element from DOM 
 	*/
 	View.prototype._removeElement = function(elementId) {
 		this.elementId = elementId;
@@ -88,8 +66,32 @@
 		parent.parentNode.removeChild(parent);
 	}
 
-	/*
-	*	Hook up our view to window object
+	/**
+	* Build uploader's user interface
+	*/
+	View.prototype.buildInterface = function(parentId) {
+		this.parentId = parentId;
+
+		this._createElement("section", "uploader-js", this.parentId);		
+		this._createElement("header", "header", "uploader-js");
+		this._createElement("section", "main", "uploader-js");
+		this._createElement("h1", "title", "header");
+		this._appendTextNode("title", "UploaderJS: Upload a file");
+		this._createElement("div", "thumbs-container", "main");
+		this._createElement("div", "droper", "main");
+		this._createElement("h2", "subtitle", "droper");
+		this._appendTextNode("subtitle", "Drop your .png or .jpg files here");
+		this._createElement("div", "file-uploader", "main");
+		this._createElement("span", "or", "file-uploader");
+		this._appendTextNode("or", "or");
+		this._createElement("input", "upload-btn", "file-uploader");
+		this._addAttribute("upload-btn", "type", "button");
+		this._addAttribute("upload-btn", "name", "upload-btn");
+		this._addAttribute("upload-btn", "value", "Select Files");
+	};
+
+	/**
+	* Hook up our view to window object
 	*/	
 	window.View = View;
 
