e849497d7ee72509bdb2598a6714a7d6cd5c8582
pawelqbera <pawel.kubera@chatwee.com>
2015-07-09 00:29:53 +0200

    Adding drag and drop functionallity, improve css

--- js/controller.js
+++ js/controller.js
@@ -6,12 +6,13 @@
 	*/
 	var Controller = function(view) {
 		this.view = view;
-		var _that = this;
 		var uploadBtn = document.getElementById("upload-btn");
+		var dropArea = document.getElementById("droper");
 
-	 	uploadBtn.addEventListener("click", function() {
-	 		_that.openFileDialog();
-	 	});
+	 	uploadBtn.addEventListener("click", this.openFileDialog.bind(this), false);
+		dropArea.addEventListener("dragover", this.fileDragHover.bind(this), false);
+		dropArea.addEventListener("dragleave", this.fileDragHover.bind(this), false);			
+		dropArea.addEventListener("drop", this.fileSelectHandler.bind(this), false);
 	};
 
 	/**
@@ -19,33 +20,52 @@
 	*/
 	Controller.Config = {
 		supportedMimeTypes: ['image/png','image/jpg']
-	}
+	}	
 
 	/**
 	* Fire the click event on a pseudo-file input and open the multiple file dialog 
 	*/
 	Controller.prototype.openFileDialog = function() {
-		var _that = this;
-
 	 	this.view._createElement("div", "pseudo-container", "file-uploader");
 	 	this.view._createElement("input", "pseudo-file-input", "pseudo-container");
 		this.view._addAttribute("pseudo-file-input", "type", "file");
 		this.view._addAttribute("pseudo-file-input", "multiple", "multiple");
 
- 		document.getElementById("pseudo-file-input").click();
- 		document.getElementById("pseudo-file-input").addEventListener("change", function () {
- 			_that.uploadImage(this);
- 		});
+		var pseudoFile = document.getElementById("pseudo-file-input"); 
+ 		pseudoFile.click();
+ 		pseudoFile.addEventListener("change", this.fileSelectHandler.bind(this), false);
 
  		this.view._removeElement("pseudo-container");
 	};
 
 	/**
+	* Handles file drag hover event on dragover or dragleave 
+	*/
+	Controller.prototype.fileDragHover = function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+	};
+
+	/**
+	* File selection 
+	*/
+	Controller.prototype.fileSelectHandler = function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+
+		// fetch FileList object
+		var files = e.target.files || e.dataTransfer.files;
+
+		this.uploadImage(files);
+	};
+
+	/**
 	* Start uploading all selected files 
 	*/
-	Controller.prototype.uploadImage = function(file) {
- 		for (var i = 0; i < file.files.length; i++) {			
- 			var loadedFile = file.files[i];
+	Controller.prototype.uploadImage = function(files) {
+ 		
+ 		for (var i = 0; i < files.length; i++) {			
+ 			var loadedFile = files[i];
 
  			this.renderImage(loadedFile);					
  		}		
@@ -62,7 +82,7 @@
  		}
 
  		this.reader = new FileReader();
- 		this.reader.onload = this.createThumb;
+ 		this.reader.onload = this.createThumb.bind(this);
  		this.reader.onerror = this.errorHandler; 
  		this.reader.readAsDataURL(this.file);
 	};
@@ -77,7 +97,6 @@
 			console.warn('This file extension appears to be not supported');
 			return false;
 		} else {
-			console.log('This file extension is supported');
 			return true;
 		}
 	};	
@@ -86,29 +105,23 @@
 	* Creating imgage tags with sources coressponding to all uploaded images 
 	* and appending them to a container as thumbnails 
 	*/
-	Controller.prototype.createThumb = function(e) {		
-		var _that = this;		
-
+	Controller.prototype.createThumb = function(e) {
 		var thumbnailsContainer = document.getElementById("thumbs-container");
 		var url = e.target.result;
-
  		this.thumb = document.createElement("img");
  		this.thumb.setAttribute("class", "thumb");
  		this.thumb.setAttribute("src", url);
  		thumbnailsContainer.appendChild(this.thumb);
 
-		this.thumb.addEventListener('click', function() {
-			_that.thumbnailClick(this);
-		});
+		this.thumb.addEventListener('click', this.thumbnailClick, false);
  	};
 
 	/**
 	* Handles the click event on a thumbnail by openning the image in original
 	* sizes in separate browser's window
 	*/
-	FileReader.prototype.thumbnailClick = function(thumbnail) {	
-		this.thumbnail = thumbnail;
-		var url = thumbnail.getAttribute("src");
+	Controller.prototype.thumbnailClick = function() {
+		var url = this.getAttribute("src");
  		var win = window.open("", "Title", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=580, height=200");		
  		win.document.body.innerHTML = ("<img src='" + url + "' />");
  	};
@@ -117,7 +130,6 @@
 	* Handles all upload errors resulted from broken and not-readable image files 
 	*/
 	Controller.prototype.errorHandler = function(e) {
-
 		if(e.target.error.name == "NotReadableError") {		
 			console.log("The file could not be read");		
 		}
